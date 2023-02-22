6b6acde298b7dcbcb6dfc6178b792ce954a274c5
Alexandr <jwchaganav@gmail.com>
2023-02-22 08:55:40 +0400

    9 заданий

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,32 +16,45 @@ public class Main {
         for(String flower: list){
             System.out.println(flower);
         }
+        System.out.println("-".repeat(15));
         // 2) Итерация
         List<String> list2 = new ArrayList<>();
         for(String flower: list){
             list2.add(flower+"!");
         }
-        System.out.println(list2);
+        System.out.println(list);
         // 3) Вставка элемента
         list.add(0,"Фиалка");
         System.out.println(list);
+        System.out.println("-".repeat(15));
         // 4) Извлечь элемент
         System.out.println(list2.get(1));
+        System.out.println("-".repeat(15));
         // 5) Обновить элемент
+
         list2.remove(2);
         list2.add(2,"Орхидея");
         System.out.println(list2);
+        System.out.println("-".repeat(15));
         // 6) Удаление третьего элемента
         list2.remove(2);
         System.out.println(list2);
+        System.out.println("-".repeat(15));
         // 7) Поиск элемента
         for(String flower: list) {
             if(flower.equals("Тюльпан"))
                 System.out.println(flower);
         }
+        System.out.println("-".repeat(15));
         // 8) Новый список
         List<String> flowers = new ArrayList<>();
-        flowers.retainAll(list);
+        for(int i=0;i<list.size()-1;i=i+2)
+            flowers.add(list.get(i));
         System.out.println(flowers);
+        System.out.println("-".repeat(15));
+        // 9) Удалить из списка
+        list.removeAll(flowers);
+        System.out.println(list);
+        //
     }
 }
